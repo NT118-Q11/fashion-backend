bee8e4071df3f8aa67ee20886af41145ae3c4be2
kenn <22520637@gm.uit.edu.vn>
2025-11-30 14:41:01 +0700

    refactor: remove demo application code and rename project to fashion backend
    - Deleted all demo-related Java classes including application, configs, controllers, DTOs, models, repositories, and services
    - Updated project description in build.gradle from 'Demo project for Spring Boot' to 'Fashion Backend project for Spring Boot'
    - Changed spring.application.name in application.properties from 'demo' to 'fashion

--- src/main/java/NT5118/Q11_backend/demo/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package NT5118.Q11_backend.demo.service;
-
-import NT5118.Q11_backend.demo.dto.UserRegistrationRequest;
-import NT5118.Q11_backend.demo.model.User;
-
-public interface UserService {
-    User register(UserRegistrationRequest request);
-}
