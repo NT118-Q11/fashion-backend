a414ef4d8e320900334471f246fffdf30a0367e2
Thanh Tung <nguyenthanhtung020405@gmail.com>
2025-12-20 13:35:50 +0700

    Saving firstName, lastName in database

--- src/main/java/NT5118/Q11_backend/fashion/user/model/User.java
+++ src/main/java/NT5118/Q11_backend/fashion/user/model/User.java
@@ -20,16 +20,22 @@ public class User {
     @Indexed(unique = true)
     private String phoneNumber = "0";
 
+    private String firstName;
+
+    private String lastName;
+
     private String userAddress;
 
 
     // constructors, getters, setters
     public User() {}
-    public User(String username, String email, String password, String phoneNumber) {
+    public User(String username, String email, String password, String phoneNumber, String firstName, String lastName) {
         this.username = username;
         this.email = email;
         this.password = password;
         this.phoneNumber = phoneNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 
     public String getId() { return id; }
@@ -46,6 +52,12 @@ public class User {
 
     public String getPhoneNumber() { return phoneNumber;}
     public void setPhoneNumber(String phoneNumber) {this.phoneNumber = phoneNumber;}
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
 
     public String getUserAddress() { return userAddress; }
     public void setUserAddress(String userAddress) { this.userAddress = userAddress; }
